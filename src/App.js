6f229a246ae7a6991f28eef191ce311d5252bf81
FolusoSAMUEL <folusosamuel@gmail.com>
2023-07-26 13:22:59 +0100

    Project Layout

---
+++ src/App.js
@@ -0,0 +1,38 @@
+function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip 🤷‍♂️</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        👌You have X items on your list, and you already packed x (xpercent)
+      </em>
+    </footer>
+  );
+}
+
+export default App;
