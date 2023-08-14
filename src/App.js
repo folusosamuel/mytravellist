27d7626a101130abe4c70728f8dffbf53f58cf1e
FolusoSAMUEL <folusosamuel@gmail.com>
2023-08-14 13:25:06 +0100

    Calculating statistics form Derive state

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -119,11 +119,24 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="footer">
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
       <em>
-        👌You have X items on your list, and you already packed x (xpercent)
+        {percentage === 100
+          ? `You got everything! Ready to go 👌`
+          : `You have ${numItems} items on your list, and you already packed
+      ${numPacked} (${percentage})%`}
       </em>
     </footer>
   );
